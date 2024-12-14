8791ef672692d90b295be6e51bc3ae390d4fa846
David Guentelican <david.guentelican@gmail.com>
2024-12-14 12:28:01 -0300

    clase editorial ok

--- biblioteca_ecd_pr/core_app/models.py
+++ biblioteca_ecd_pr/core_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+# MODELO DE CLASES ORM
+
+#Editorial
+class Editorial(models.Model):
+    id_editorial = models.SmallAutoField(primary_key=True)
+    nombre_editorial = models.CharField(unique=True, max_length=40) #unique, para evitar que se repita
+
+    def __str__(self):
+        return self.nombre_editorial
