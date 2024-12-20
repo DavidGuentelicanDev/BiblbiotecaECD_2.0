6dee9df2a671f7dc34e6570b9797828f597f7315
David Guentelican <david.guentelican@gmail.com>
2024-12-20 15:57:05 -0300

    validacion de campos vacios de login con bootstrap

---
+++ biblioteca_ecd_pr/ecd_admin_app/static/js/login.js
@@ -0,0 +1,17 @@
+//Validacion de bootstrap para los campos vacios
+(() => {
+    'use strict'
+
+    const forms = document.querySelectorAll('.needs-validation')
+
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+
+        form.classList.add('was-validated')
+        }, false)
+    });
+})()
