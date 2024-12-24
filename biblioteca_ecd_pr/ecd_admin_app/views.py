0d19a93d45bd801c24fcc4ec29576db5a61b007a
David Guentelican <david.guentelican@gmail.com>
2024-12-24 14:22:38 -0300

    funciones para delimitar los permisos de acceso segun rol o is_staff

--- biblioteca_ecd_pr/ecd_admin_app/views.py
+++ biblioteca_ecd_pr/ecd_admin_app/views.py
@@ -1,15 +1,17 @@
-from django.shortcuts import render
-from core_app.models import Usuario
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
+from django.contrib.auth.decorators import login_required, user_passes_test
 
 
-#Login, permisos y Logout
+#Login, Permisos y Logout
 
 #pagina login
 def inicio_sesion_page(request):
+    if request.user.is_authenticated:
+        return redirect('principal')
     return render(request, 'ecd_admin_app/login.html')
 
 #login
@@ -40,10 +42,24 @@ def adm_logout(request):
     logout(request)
     return JsonResponse({"status": "success"})
 
+#permisos todo el staff
+def staff_requerido(user):
+    return user.is_staff
+
+#permisos solo bibliotecario y admin
+def bibliotecario_requerido(user):
+    return user.rol in [1, 2]
+
+#permisos solo admin
+def administrador_requerido(user):
+    return user.rol == 1
+
 ######################################################################################################################################
 
 #Home
 
 #pagina principal
+@login_required
+@user_passes_test(staff_requerido)
 def home(request):
     return render(request, 'ecd_admin_app/home.html')
